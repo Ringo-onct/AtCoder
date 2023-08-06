428d1d8b90efd15e2e355701de8d1a53cd80169b
Ringo-onct <tomatosan114514@gmail.com>
2023-08-06 15:24:09 +0900

    [AC] 正解
    コード長	756 Byte
    実行時間	56 ms
    メモリ	3432 KB
    
    これは模範解答をCで書き直したもの。
    これ難しかった、、

--- atcoder-beginner-contest-313/approximate-equalization2.c
+++ atcoder-beginner-contest-313/approximate-equalization2.c
@@ -1,36 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef long long ll;
+
+int cmpfunc(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
 
 int main() {
-    int A[200000];
-    int N;
-    int i;
-    int min = 0;
-    int max = 0;
-    long int x;
-    int d;
-
-    scanf("%d", &N);
-
-    for (i = 0; i < N; i++) {
-        scanf("%d", &A[i]);
-        if (A[min] >= A[i]) {
-            min = i;
-        } else if (A[max] <= A[i]) {
-            max = i;
-        }
+    int n;
+    scanf("%d", &n);
+    int *a = (int *)malloc(sizeof(int) * n);
+    ll sum = 0;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+        sum += a[i];
     }
-
-    for (x = 0; A[max] - A[min] > 1; x++) {
-        A[max]--;
-        A[min]++;
-        for (i = 0; i < N; i++) {
-            if (A[min] >= A[i]) {
-                min = i;
-            } else if (A[max] <= A[i]) {
-                max = i;
-            }
-        }
+    qsort(a, n, sizeof(int), cmpfunc);
+    int *b = (int *)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++) {
+        b[i] = sum / n;
     }
-
-    printf("%ld\n", x);
+    for (int i = 0; i < sum % n; i++) {
+        b[n - 1 - i]++;
+    }
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        ans += abs(a[i] - b[i]);
+    }
+    printf("%lld\n", ans / 2);
+    free(a);
+    free(b);
+    return 0;
 }
